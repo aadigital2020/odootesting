01a08283cbb5cb48d97478073fb878639b2b9925
Your Name <you@example.com>
2020-12-18 18:58:48 +0800

    update

--- cortana/__manifest__.py
+++ cortana/__manifest__.py
@@ -13,22 +13,12 @@
     'author': "A&A Limited",
     'website': "http://www.aa-limited.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Test',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'web'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
+        'views/assets.xml',
     ],
 }
