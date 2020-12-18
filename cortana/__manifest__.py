0e0df9392af4083f95c74e3749176fa6fb682d2b
Your Name <you@example.com>
2020-12-18 18:15:14 +0800

    create cortana module

---
+++ cortana/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cortana",
+
+    'summary': """
+        Cortana
+    """,
+
+    'description': """
+        Cortana
+    """,
+
+    'author': "A&A Limited",
+    'website': "http://www.aa-limited.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
