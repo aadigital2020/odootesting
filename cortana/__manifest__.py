30aabca7d36880880ce5d71658339a4246f6cd37
Noah Cheng <noahcheng@aa-limited.com>
2020-12-23 11:38:27 +0800

    Revert "update"
    This reverts commit 4272ca5f1f42434e683e9822a4482b6f2ebaf60e.

---
+++ cortana/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cortana",
+
+    'summary': """Cortana2""",
+
+    'description': """Cortana2""",
+
+    'author': "A&A Limited",
+    'website': "http://www.aa-limited.com",
+
+    'category': 'Test',
+    'version': '0.1',
+
+    'depends': ['base', 'web'],
+
+    'data': [
+        'views/assets.xml',
+    ],
+}
---
+++ cortana/static/src/js/cortana.js
@@ -0,0 +1,5 @@
+$(function() {
+    $(document).ajaxComplete(function() {
+        console.log('Ajax call completed');
+    });
+});
