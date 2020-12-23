c52ad5d8c0b9998350169f8087f6a58216eeb342
Noah Cheng <noahcheng@aa-limited.com>
2020-12-23 14:36:48 +0800

    update

--- cortana/controllers.py
+++ cortana/controllers.py
@@ -2,6 +2,6 @@
 from odoo import http
 
 class Cortana(http.Controller):
-    @http.route('/cortana/export/', auth='public')
+    @http.route('/cortana/export/', type='json', auth='public')
     def handler(self, **kw):
         return 'Hello, world'
