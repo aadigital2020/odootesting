c24798f41c08c684771e5108cb3ff594ef2b03e1
Noah Cheng <noahcheng@aa-limited.com>
2020-12-23 15:34:19 +0800

    update

--- cortana/controllers.py
+++ cortana/controllers.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from odoo import http
-import simplejson
 
 class Cortana(http.Controller):
     @http.route('/cortana/cortana', type='http', auth='user')
     def handler(self, **kw):
-        return request.make_response(simplejson.dumps({"response": "OK"}), [('Content-Type', 'application/json')])
+        return {
+            'test' : 'test'
+        }
 
     @http.route('/cortana/cortana/example/ajax', type='json', auth='user')
     def handler(self, **kw):
