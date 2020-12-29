b75272c4b42cf3f619a3b6408d8c74b65ebacbbb
Noah Cheng <noahcheng@aa-limited.com>
2020-12-29 11:03:46 +0800

    update

--- cortana/controllers.py
+++ cortana/controllers.py
@@ -7,18 +7,12 @@ class Cortana(http.Controller):
     def test(self, **kw):
         models = http.request.env['x_this_is_tour_123'].search([]) # Change model name
 
+        # Print models
         raw_data = models.read()
+        return raw_data
         json_data = json.dumps(raw_data, indent=4, sort_keys=True, default=str)
         return json_data
-        
-        # for model in models:
-        #     results = model.read()
-
-        return print(models)
-
-        data = json.dumps(models)
-        # return len(models)
-
+    
     @http.route('/cortana/cortana/export', type='http', auth='user')
     def export(self, **kw):
         data = json.dumps(
