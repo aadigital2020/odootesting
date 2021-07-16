c4fc64e8991170daa5b8e32b5dbfba1d81ca7772
Noah Cheng <noahcheng@aa-limited.com>
2021-07-16 09:49:31 +0800

    init

--- cortana/static/src/js/cortana.js
+++ cortana/static/src/js/cortana.js
@@ -71,36 +71,48 @@ odoo.define('cortana__export_button.listview_button', function (require) {
     $(function() {
         $('body').append(`
         <style>
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(8),
         html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(9),
         html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(10),
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(11),
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(8),
         html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(9),
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(10),
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(11) {
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(10) {
             background-color: #c1d0fc;
         }
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(11),
         html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(12),
         html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(13),
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(14),
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(11),
         html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(12),
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(13),
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(14) {
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(13) {
             background-color: #fbc8d5;
         }
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(16),
-        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(16) {
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(14),
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(15),
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(14),
+        html[data-app-model="x_this_is_tour_123"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(15) {
             background-color: #c8e5c7;
         }
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(9),
         html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(10),
-        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(10) {
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(11),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(9),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(10),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(11) {
             background-color: #c1d0fc;
         }
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(12),
         html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(13),
-        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(13) {
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(14),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(12),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(13),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(14) {
             background-color: #fbc8d5;
         }
-        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(17),
-        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(17) {
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(15),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] th:nth-child(15),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(16),
+        html[data-app-model="x_in_bound_tour"][data-app-view-type="list"][data-app-studio="ok"] td:nth-child(16) {
             background-color: #c8e5c7;
         }
         </style>
